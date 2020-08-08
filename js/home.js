31c191d9c17537f53a497607b97067c992ded811
Ankit saha <ankitsahas18@gmail.com>
2020-08-09 01:29:59 +0530

    update home

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
-$(".button-bottom-b1").ready(function () {//for the button at bottom of header
+var x = 0;
+$(".button-bottom-b1").ready(function () { //for the button at bottom of header
     $(".button-bottom-b1").css("border-style", "solid");
     $(".button-bottom-b1").css("border-width", "5px");
     $(".button-bottom-b1").css("border-color", "white");
@@ -7,4 +8,15 @@ $(".button-bottom-b1").ready(function () {//for the button at bottom of header
     $(".button-bottom-b1").css("background", "white");
     $(".button-bottom-b1").css("color", "#ac00e6");
     $(".button-bottom-b1").css("font-weight", "500");
+});
+$(".name").html("ankit");
+$(".like").click(function () {
+    if(x==1){
+        $(".like").css("background-image", 'url("../icon/ok_52px.png")');
+        x=0;
+    }
+    else{
+        $(".like").css("background-image", 'url("../icon/ok_100px.png")'); 
+        x=1;
+    }
 });
